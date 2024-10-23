69b928c65b917d0cbe2e4af0bbbe00eabc78b92b
SUKU2512 <105798769+SUKU2512@users.noreply.github.com>
2024-10-23 17:43:42 +0530

    Tailwind Project
    You can directly without using vs code

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '576px',
+
+      'md': '768px',
+
+      'lg': '992px',
+      
+      'xl': '1200px',
+  
+    },
+    width:{
+      35:"35rem",
+    }
+  }
+}
